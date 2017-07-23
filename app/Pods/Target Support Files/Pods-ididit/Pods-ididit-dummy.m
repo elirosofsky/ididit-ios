9eaadfdbec822d5289a5e5c3d25bd18db79a6063
Jay Clark <jason.clark@raizlabs.com>
2017-07-22 20:28:45 -0400

    project set up

---
+++ app/Pods/Target Support Files/Pods-ididit/Pods-ididit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ididit : NSObject
+@end
+@implementation PodsDummy_Pods_ididit
+@end
---
+++ app/ididit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ididit
+//
+//  Created by Jason Clark on 7/22/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
